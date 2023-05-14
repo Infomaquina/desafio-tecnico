f48d8eb4680d26f4ccea4d91dbe117579a064591
Brandev <brandeveloper@outlook.br>
2023-05-14 09:13:42 -0300

    ✨ Project Link on header

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,5 +1,5 @@
 <div class="row">
   <div class="mt-3 text-center animate__animated animate__backInDown">
-    <img src="/<?=IMG?>PNG.png" alt="" width="50px">
+    <a href="https://github.com/Infomaquina/desafio-tecnico" target="_blank" title="ACESSAR O PROJETO"><img src="/<?=IMG?>PNG.png" alt="" width="50px"></a>
   </div>
 </div>
